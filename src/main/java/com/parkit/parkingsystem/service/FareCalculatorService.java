6fe98755112a1dc538bff4a0ff0decce71766ad2
Frederic GSN <fredgsn59@gmail.com>
2020-09-10 10:53:14 +0200

    TODO#2

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,6 +3,7 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.time.Duration;
 import java.util.Calendar;
 
 public class FareCalculatorService {
@@ -15,30 +16,31 @@ public class FareCalculatorService {
         // ancienne syntaxe :
         //int inHour = ticket.getInTime().getHours();
         // remplacé par ;
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(ticket.getInTime());
-        int inHour = calendar.get(Calendar.HOUR_OF_DAY);
+        //Calendar calendar = Calendar.getInstance();
+        //calendar.setTime(ticket.getInTime());
+        //int inHour = calendar.get(Calendar.HOUR_OF_DAY);
 
         // ancienne syntaxe :
         //int outHour = ticket.getOutTime().getHours();
         // remplacé par ;
-        calendar.setTime(ticket.getOutTime());
-        int outHour = calendar.get(Calendar.HOUR_OF_DAY);
+        //calendar.setTime(ticket.getOutTime());
+        //int outHour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        // duration : temps en ms
+        Duration duration = Duration.between(ticket.getInTime().toInstant(),ticket.getOutTime().toInstant());
+
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration;
-        if (outHour>inHour)
-            duration = outHour - inHour;
-        else
-            duration = 24 - inHour + outHour; // Correction si outHour < inHour
+
+
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(duration.toMinutes()/60F * Fare.CAR_RATE_PER_HOUR); //F : flottant
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(duration.toMinutes()/60F * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,15 +1,15 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -37,9 +37,9 @@ public class ParkingDataBaseIT {
 
     @BeforeEach
     private void setUpPerTest() throws Exception {
-        when(inputReaderUtil.readSelection()).thenReturn(1);
+        when(inputReaderUtil.readSelection()).thenReturn(1); // 1 : voiture
         when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-        dataBasePrepareService.clearDataBaseEntries();
+        dataBasePrepareService.clearDataBaseEntries(); // clear et reprépare Vl
     }
 
     @AfterAll
@@ -50,16 +50,55 @@ public class ParkingDataBaseIT {
     @Test
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+        // On récupère la prochaine place disponible
+        int nextPlace = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+
         parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        //vérifier qu'un ticket est effectivement enregistré dans la base de données et que la table de
+        // stationnement est mise à jour avec la disponibilité
+
+        // étape 1 : on récupère le ticket du véhicule immatriculé ABCDEF
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+
+        // étape 2 : on vérifie l'existance du ticket ( not Null)
+        Assertions.assertNotNull(ticket);
+
+        // étape 3 : on récupère id parking et on vérifie que ParkingSpot existe bien
+        ParkingSpot parkingSpot = ticket.getParkingSpot();
+        Assertions.assertNotNull(parkingSpot);
+
+        // étape 4 : on vérifie que la colonne avalaible = false
+        Assertions.assertFalse(parkingSpot.isAvailable());
+
+        // étape 5 : on vérifie que la place qui était disponible est bien celle utilisée
+        Assertions.assertEquals(nextPlace, parkingSpot.getId());
     }
 
     @Test
-    public void testParkingLotExit(){
+    public void testParkingLotExit() throws InterruptedException {
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+        Thread.sleep(1000); // On simule un temps entre entrée et sortie pour eviter l'exeption
+
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
+        //vérifier que le tarif généré et l'heure de départ sont correctement renseignés dans la base de données
+
+        // étape 1 : on récupère le ticket du véhicule immatriculé ABCDEF
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+
+        // étape 2 : on vérifie l'existance du ticket ( not Null)
+        Assertions.assertNotNull(ticket);
+
+        // étape 3 : on vérifie que le ticket à bien une date de sortie (outTime)
+        ParkingSpot parkingSpot = ticket.getParkingSpot();
+        Assertions.assertNotNull(ticket.getOutTime());
+
+        // étape 4 : on vériefie si le tarif du ticket est correct
+        Assertions.assertEquals( 0.0, ticket.getPrice() );
     }
 
 }
