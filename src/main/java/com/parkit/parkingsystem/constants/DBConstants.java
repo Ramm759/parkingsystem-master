d068cbd5c8959c68d185faa25ecfeee67c8a0211
Frederic GSN <fredgsn59@gmail.com>
2020-09-14 17:33:31 +0200

    Revert "STORY#1"
    This reverts commit bfbb6b75a334b85112bbb1c0d9e451b3ac4abd12.

--- src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
@@ -8,9 +8,4 @@ public class DBConstants {
     public static final String SAVE_TICKET = "insert into ticket(PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME) values(?,?,?,?,?)";
     public static final String UPDATE_TICKET = "update ticket set PRICE=?, OUT_TIME=? where ID=?";
     public static final String GET_TICKET = "select t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.IN_TIME  limit 1";
-
-    // #STORY2 : renvoie zéro si le client n'a aucun enregistrement, elle renvoie un nombre différent de 0 si le client a déjà payé au moins une fois
-    // on supprime également les durées < 30 minutes
-    public static final String IS_REGULAR_CUSTOMER = " select count(t.ID)>0 from ticket t where t.VEHICLE_REG_NUMBER=? and t.PRICE>0";
-
 }
--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -8,43 +8,42 @@ import java.util.Calendar;
 
 public class FareCalculatorService {
 
-    // STORY#2
-    public void calculateFare(Ticket ticket, double discount){
-        calculateFare(ticket);
-        ticket.setPrice(ticket.getPrice() - (ticket.getPrice() * discount / 100F)) ;
-        // ticket.setPrice(ticket.getPrice() * (1 - discount / 100F)) ;
-    }
-
-    public void calculateFare(Ticket ticket) {
-        if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
-            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+    public void calculateFare(Ticket ticket){
+        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
+            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        // TODO#1 : Correction du code pour passer les tests
+        // ancienne syntaxe :
+        //int inHour = ticket.getInTime().getHours();
+        // remplacé par ;
+        //Calendar calendar = Calendar.getInstance();
+        //calendar.setTime(ticket.getInTime());
+        //int inHour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        // ancienne syntaxe :
+        //int outHour = ticket.getOutTime().getHours();
+        // remplacé par ;
+        //calendar.setTime(ticket.getOutTime());
+        //int outHour = calendar.get(Calendar.HOUR_OF_DAY);
 
         // duration : temps en ms
-        Duration duration = Duration.between(ticket.getInTime().toInstant(), ticket.getOutTime().toInstant());
+        Duration duration = Duration.between(ticket.getInTime().toInstant(),ticket.getOutTime().toInstant());
 
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
 
-        // STORY#1 : 30 minutes gratuites
-        if (duration.toMinutes() <= 30)
-            ticket.setPrice(0.0);
 
-        else {
-           switch (ticket.getParkingSpot().getParkingType()) {
-                case CAR: {
-                    ticket.setPrice(duration.toMinutes() / 60F * Fare.CAR_RATE_PER_HOUR); //F : flottant
-                    break;
-                }
-                case BIKE: {
-                    ticket.setPrice(duration.toMinutes() / 60F * Fare.BIKE_RATE_PER_HOUR);
-                    break;
-                }
-                default:
-                    throw new IllegalArgumentException("Unkown Parking Type");
+
+        switch (ticket.getParkingSpot().getParkingType()){
+            case CAR: {
+                ticket.setPrice(duration.toMinutes()/60F * Fare.CAR_RATE_PER_HOUR); //F : flottant
+                break;
             }
+            case BIKE: {
+                ticket.setPrice(duration.toMinutes()/60F * Fare.BIKE_RATE_PER_HOUR);
+                break;
+            }
+            default: throw new IllegalArgumentException("Unkown Parking Type");
         }
     }
 }
